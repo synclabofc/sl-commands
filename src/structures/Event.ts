7c77178b82dd0bc5dd96e135960d1e8938ab43f6
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-08-18 10:26:09 -0300

    Added support for the handler events

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -1,14 +1,28 @@
 import { Client, ClientEvents } from 'discord.js'
 import SLHandler from '..'
+import { HandlerEvents } from '../types'
 
-export class SLEvent<K extends keyof ClientEvents> {
+export class SLEvent<K extends keyof HandlerEvents | keyof ClientEvents> {
+	/**
+	 * Creates a SLEvent
+	 *
+	 * @param name - The event name (key of DiscordJS ClientEvents)
+	 * @param callback - The function which will be executed when the event is emitted
+	 */
 	constructor(
 		public name: K,
 		public callback: (
-			client: Client,
-			handler: SLHandler,
-			...args: ClientEvents[K]
-		) => any
+			ctx: {
+				client: Client
+				handler: SLHandler
+			},
+			...args: K extends keyof HandlerEvents
+				? Parameters<HandlerEvents[K]>
+				: K extends keyof ClientEvents
+				? ClientEvents[K]
+				: never
+		) => any,
+		public once?: boolean
 	) {
 		if (!name || !callback) {
 			throw new TypeError(
