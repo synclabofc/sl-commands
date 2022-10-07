fda7a4ad474db5099c8ef2caf4149399f4bb3227
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-10-07 18:27:56 -0300

    Look for updates build

--- dist/LookForUpdates.js
+++ dist/LookForUpdates.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const package_json_1 = tslib_1.__importDefault(require("package-json"));
+const latest_version_1 = tslib_1.__importDefault(require("latest-version"));
 const semver_1 = tslib_1.__importDefault(require("semver"));
 const chalk_1 = tslib_1.__importDefault(require("chalk"));
 const boxen_1 = tslib_1.__importDefault(require("boxen"));
 const fs_1 = tslib_1.__importDefault(require("fs"));
 async function lookForUpdates() {
-    const { dependencies } = JSON.parse(fs_1.default.readFileSync('../package.json'));
-    const currentVersion = dependencies['sl-commands'];
-    const { version: latestVersion } = await (0, package_json_1.default)('sl-commands');
+    const { dependencies } = JSON.parse(fs_1.default.readFileSync(__dirname + '/../package.json'));
+    const currentVersion = dependencies['sl-commands'].replace(/[^0-9.]/g, '');
+    const latestVersion = await (0, latest_version_1.default)('sl-commands');
     const updateAvailable = semver_1.default.lt(currentVersion, latestVersion);
     if (updateAvailable) {
         const msg = {
@@ -17,6 +17,8 @@ async function lookForUpdates() {
             runUpdate: `Run ${chalk_1.default.cyan(`npm i sl-commands@latest`)} to update`,
         };
         console.log((0, boxen_1.default)(`${msg.updateAvailable}\n${msg.runUpdate}`, {
+            borderColor: 'cyanBright',
+            title: 'sl-commands',
             align: 'center',
             padding: 1,
             margin: 1,
